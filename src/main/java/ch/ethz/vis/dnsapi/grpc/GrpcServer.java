8f978e6539296e4118dfb47f5d944948e390c3e8
Jan Veen <veenj@vis.ethz.ch>
2019-08-20 13:27:18 +0200

    Refactor GrpcServer
    Split into small methods

--- src/main/java/ch/ethz/vis/dnsapi/grpc/GrpcServer.java
+++ src/main/java/ch/ethz/vis/dnsapi/grpc/GrpcServer.java
@@ -2,21 +2,23 @@ package ch.ethz.vis.dnsapi.grpc;
 
 import ch.ethz.vis.dnsapi.netcenter.NetcenterAPI;
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import io.grpc.protobuf.services.ProtoReflectionService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.net.SocketAddress;
 
 public class GrpcServer {
-    private static final Logger LOGGER = LogManager.getLogger(GrpcServer.class);
+
+    private static final Logger LOG = LogManager.getLogger(GrpcServer.class);
+
+    private NetcenterAPI netcenterAPI;
 
     private Server server;
-    private NetcenterAPI netcenterAPI;
+
     private int port;
+
     private String defaultIsg;
 
     public GrpcServer(NetcenterAPI netcenterAPI, String defaultIsg) {
@@ -26,22 +28,23 @@ public class GrpcServer {
     }
 
     public void serve() throws IOException {
-        server = NettyServerBuilder.forPort(port)
+        server = instantiateServer();
+        server.start();
+        joinServer();
+    }
+
+    private Server instantiateServer() {
+        return NettyServerBuilder.forPort(port)
                 .addService(new DnsImpl(netcenterAPI, defaultIsg))
                 .addService(ProtoReflectionService.newInstance())
                 .build();
+    }
 
-        try {
-            server.start();
-        } catch (IOException e) {
-            LOGGER.error("error encountered starting server: " + e);
-            throw new RuntimeException("could not start server: " + e);
-        }
-
+    private void joinServer() {
         try {
             server.awaitTermination();
         } catch (InterruptedException e) {
-            LOGGER.info("interrupted: ", e);
+            LOG.info("interrupted", e);
         }
     }
 }
