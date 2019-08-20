00aa4f576a706f18de50e1e50600d3be297fdec1
Jan Veen <veenj@vis.ethz.ch>
2019-08-20 13:15:21 +0200

    Refactor NetcenterConfig
    Split into small methods
    Insert whitespace

--- src/main/java/ch/ethz/vis/dnsapi/NetcenterConfig.java
+++ src/main/java/ch/ethz/vis/dnsapi/NetcenterConfig.java
@@ -7,33 +7,23 @@ import org.apache.logging.log4j.Logger;
 import java.util.Properties;
 
 public class NetcenterConfig {
-    private static final Logger LOGGER = LogManager.getLogger(NetcenterConfig.class);
+
+    private static final Logger LOG = LogManager.getLogger(NetcenterConfig.class);
 
     private static final String NETCENTER_USERNAME_KEY = "ch.ethz.vis.dnsapi.netcenter.username";
+
     private static final String NETCENTER_PASSWORD_KEY = "ch.ethz.vis.dnsapi.netcenter.password";
+
     private static final String NETCENTER_ISGGROUP_KEY = "ch.ethz.vis.dnsapi.netcenter.isgGroup";
 
     private String username;
+
     private String password;
+
     private String isgGroup;
 
     public NetcenterConfig(Properties p) throws InitializationException {
         readProperties(p);
-    }
-
-    private void readProperties(Properties p) throws InitializationException {
-        this.username = p.getProperty(NETCENTER_USERNAME_KEY);
-        this.password = p.getProperty(NETCENTER_PASSWORD_KEY);
-        this.isgGroup = p.getProperty(NETCENTER_ISGGROUP_KEY);
-
-        if (this.username == null || this.password == null || this.isgGroup == null) {
-            LOGGER.error("Username, password and isgGroup must be provided!");
-            throw new InitializationException("Username, password or isgGroup not set");
-        }
-
-        LOGGER.debug("Username: " + username);
-        LOGGER.debug("Password: " + "*".repeat(password.length()));
-        LOGGER.debug("ISG Group: " + isgGroup);
     }
 
     public String getUsername() {
@@ -46,5 +36,35 @@ public class NetcenterConfig {
 
     public String getIsgGroup() {
         return isgGroup;
+    }
+
+    private void readProperties(Properties p) throws InitializationException {
+        copyPropertiesToFields(p);
+        checkConfigurationValidity();
+        printDebugInformation();
+    }
+
+    private void copyPropertiesToFields(Properties p) {
+        this.username = p.getProperty(NETCENTER_USERNAME_KEY);
+        this.password = p.getProperty(NETCENTER_PASSWORD_KEY);
+        this.isgGroup = p.getProperty(NETCENTER_ISGGROUP_KEY);
+    }
+
+    private void checkConfigurationValidity() throws InitializationException {
+        if (username == null) {
+            throw new InitializationException("username must be provided");
+        }
+        if (password == null) {
+            throw new InitializationException("password must be provided");
+        }
+        if (isgGroup == null) {
+            throw new InitializationException("isgGroup must be provided");
+        }
+    }
+
+    private void printDebugInformation() {
+        LOG.debug("Username: " + username);
+        LOG.debug("Password: " + "*".repeat(password.length()));
+        LOG.debug("ISG Group: " + isgGroup);
     }
 }
