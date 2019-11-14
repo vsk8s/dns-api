8f1981894a3dab9d4e47057f9dd37534961da8de
Ben Fiedler <bfiedler@ethz.ch>
2019-11-14 16:57:23 +0100

    Clean up tests for ARecordmanager
      * test each Response in a single method
      * remove VIS information from test data

--- src/test/java/ch/ethz/vis/dnsapi/netcenter/ARecordManagerTest.java
+++ src/test/java/ch/ethz/vis/dnsapi/netcenter/ARecordManagerTest.java
@@ -9,6 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.jaxb.JaxbConverterFactory;
 
 import javax.xml.bind.JAXBContext;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,15 +18,13 @@ public class ARecordManagerTest {
     private static MockWebServer server;
     private static ARecordManager aRecordManager;
 
-    //TODO: Remove any vis.ethz.ch TLD from tests
-
     @BeforeAll
     public static void setup() throws Exception {
         server = new MockWebServer();
         server.requireClientAuth();
 
         // Testing GET
-        server.enqueue(new MockResponse().setResponseCode(200).setBody("<usedIps><usedIp><ip>192.0.2.10</ip><ipSubnet>192.0.2.0</ipSubnet><fqname>some.server.some.tld</fqname><forward>Y</forward><reverse>Y</reverse><ttl>3600</ttl><dhcp>N</dhcp><ddns>N</ddns><isgGroup>adm-vis</isgGroup><lastDetection>2019-07-23 22:06</lastDetection><ipVersion>v4</ipVersion><views><view>extern</view><view>intern</view></views></usedIp></usedIps>"));
+        server.enqueue(new MockResponse().setResponseCode(200).setBody("<usedIps><usedIp><ip>192.0.2.10</ip><ipSubnet>192.0.2.0</ipSubnet><fqname>some.server.some.tld</fqname><forward>Y</forward><reverse>Y</reverse><ttl>3600</ttl><dhcp>N</dhcp><ddns>N</ddns><isgGroup>some-isg</isgGroup><lastDetection>2019-07-23 22:06</lastDetection><ipVersion>v4</ipVersion><views><view>extern</view><view>intern</view></views></usedIp></usedIps>"));
         server.enqueue(new MockResponse().setResponseCode(200).setBody("<usedIps></usedIps>"));
         server.enqueue(new MockResponse().setResponseCode(500).setBody("<errors><error>Some internal error occurred!</error></errors>"));
 
@@ -49,7 +48,7 @@ public class ARecordManagerTest {
 
     @Test
     @Order(1)
-    public void testGetARecord() throws Exception {
+    public void successfullyGetARecordWithRecord() throws Exception {
         Response<GetARecordResponse> response = aRecordManager.GetARecord("some.server.some.tld").execute();
         assertTrue(response.isSuccessful());
         assertNotNull(response.body());
@@ -60,7 +59,7 @@ public class ARecordManagerTest {
         assertEquals("v4", first.getIpVersion());
         assertEquals("192.0.2.10", first.getIp());
         assertEquals("192.0.2.0", first.getIpSubnet());
-        assertEquals("adm-vis", first.getIsgGroup());
+        assertEquals("some-isg", first.getIsgGroup());
         assertTrue(first.getForward());
         assertTrue(first.getReverse());
         assertFalse(first.getDdns());
@@ -68,48 +67,71 @@ public class ARecordManagerTest {
         assertTrue(first.getViews().size() == 2
                 && first.getViews().contains("intern")
                 && first.getViews().contains("extern"));
+    }
 
-        response = aRecordManager.GetARecord("doesnotexist.some.tld").execute();
+    @Test
+    @Order(2)
+    public void successfullyGetARecordWithEmptyResponse() throws IOException {
+        Response<GetARecordResponse> response = aRecordManager.GetARecord("doesnotexist.some.tld").execute();
         assertTrue(response.isSuccessful());
         assertNotNull(response.body());
         assertTrue(response.body().getRecords().isEmpty());
+    }
 
-        response = aRecordManager.GetARecord("something.some.tld").execute();
+    @Test
+    @Order(3)
+    public void errorDuringGetARecord() throws IOException {
+        Response<GetARecordResponse> response = aRecordManager.GetARecord("something.some.tld").execute();
         assertFalse(response.isSuccessful());
         assertNull(response.body());
         assertNotNull(response.errorBody());
     }
 
     @Test
-    @Order(2)
-    public void testCreateARecord() throws Exception {
-        XmlCreateARecordRequestWrapper request = new XmlCreateARecordRequestWrapper(
+    @Order(4)
+    public void successfullyCreateARecord() throws IOException {
+        XmlCreateARecordRequestWrapper request = makeCreateARecordRequest();
+
+        Response<XmlSuccess> response = aRecordManager.CreateARecord(request).execute();
+        assertTrue(response.isSuccessful());
+        assertNotNull(response.body());
+        assertEquals("Done!", response.body().getMessage());
+    }
+
+    @Test
+    @Order(5)
+    public void errorDuringCreateARecord() throws IOException {
+        XmlCreateARecordRequestWrapper request = makeCreateARecordRequest();
+
+        Response<XmlSuccess> response = aRecordManager.CreateARecord(request).execute();
+        assertFalse(response.isSuccessful());
+        assertNull(response.body());
+    }
+
+    @Test
+    @Order(6)
+    public void successfullyDeleteARecord() throws IOException {
+        Response<XmlSuccess> response = aRecordManager.DeleteARecord("192.0.2.10", "some-fqdn").execute();
+        assertTrue(response.isSuccessful());
+        assertNotNull(response.body());
+        assertEquals("Done!", response.body().getMessage());
+    }
+
+    @Test
+    @Order(7)
+    public void errorDuringDeleteARecord() throws IOException {
+        Response<XmlSuccess> response = aRecordManager.DeleteARecord("192.0.2.10", "some-fqdn").execute();
+        assertFalse(response.isSuccessful());
+        assertNull(response.body());
+    }
+
+    private XmlCreateARecordRequestWrapper makeCreateARecordRequest() {
+        return new XmlCreateARecordRequestWrapper(
                 CreateARecordRequest.Builder.newBuilder()
                         .withIp("192.0.2.10")
                         .withIpName("example-ip-record")
                         .withSubdomain("some-subdomain")
                         .withIsgGroup("some-isg-group")
                         .build());
-        Response<XmlSuccess> response = aRecordManager.CreateARecord(request).execute();
-        assertTrue(response.isSuccessful());
-        assertNotNull(response.body());
-        assertEquals("Done!", response.body().getMessage());
-
-        response = aRecordManager.CreateARecord(request).execute();
-        assertFalse(response.isSuccessful());
-        assertNull(response.body());
-    }
-
-    @Test
-    @Order(3)
-    public void testDeleteARecord() throws Exception {
-        Response<XmlSuccess> response = aRecordManager.DeleteARecord("192.0.2.10", "some-fqdn").execute();
-        assertTrue(response.isSuccessful());
-        assertNotNull(response.body());
-        assertEquals("Done!", response.body().getMessage());
-
-        response = aRecordManager.DeleteARecord("192.0.2.10", "some-fqdn").execute();
-        assertFalse(response.isSuccessful());
-        assertNull(response.body());
     }
 }
